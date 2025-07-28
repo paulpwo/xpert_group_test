fa73af6a7566f12cebf24ddecb49093e78051a8a
Paul Osinga <paulpwo@gmail.com>
2025-07-28 18:30:09 -0500

    feat: add WikipediaView widget to display web content with loading indicator

---
+++ lib/views/wikipedia_view.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WikipediaView extends StatefulWidget {
+  final String title;
+  final String url;
+
+  const WikipediaView({
+    super.key,
+    required this.title,
+    required this.url,
+  });
+
+  @override
+  State<WikipediaView> createState() => _WikipediaViewState();
+}
+
+class _WikipediaViewState extends State<WikipediaView> {
+  late final WebViewController controller;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Actualizar progreso de carga
+          },
+          onPageStarted: (String url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: controller),
+          if (isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
+    );
+  }
+}
