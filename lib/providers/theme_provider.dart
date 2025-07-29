0302197ba36f7fc17db9700f1bb4ab87dc88e531
Paul Osinga <paulpwo@gmail.com>
2025-07-28 19:13:24 -0500

    feat: enhance ThemeProvider to support persistent theme storage using SharedPreferences

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool _isDarkMode = false;
+  static const String _darkModeKey = 'dark_mode';
 
   bool get isDarkMode => _isDarkMode;
 
-  void toggleTheme() {
-    _isDarkMode = !_isDarkMode;
+  ThemeProvider() {
+    _loadThemeFromStorage();
+  }
+
+  Future<void> _loadThemeFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_darkModeKey) ?? false;
     notifyListeners();
   }
 
-  void setTheme(bool isDark) {
-    _isDarkMode = isDark;
+  Future<void> toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    await _saveThemeToStorage();
     notifyListeners();
+  }
+
+  Future<void> setTheme(bool isDark) async {
+    _isDarkMode = isDark;
+    await _saveThemeToStorage();
+    notifyListeners();
+  }
+
+  Future<void> _saveThemeToStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_darkModeKey, _isDarkMode);
   }
 }
