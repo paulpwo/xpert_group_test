2f9e771bcf6cb816e4309029e4d362397d517b01
Paul Osinga <paulpwo@gmail.com>
2025-07-28 19:12:57 -0500

    feat: create MainView widget to manage navigation between Home, Voting, and Settings views

---
+++ lib/modules/main/main_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'home/home_view.dart';
+import 'settings/settings_view.dart';
+import 'voting/voting_view.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  int _selectedIndex = 0;
+  late PageController _pageController;
+
+  final List<Widget> _pages = [
+    const HomeView(),
+    const VotingView(),
+    const SettingsView(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        // Deshabilitar el swipe del PageView para evitar interferencia con el CardSwiper
+        physics: const NeverScrollableScrollPhysics(),
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 28),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.how_to_vote, size: 28),
+            label: 'Votación',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings, size: 28),
+            label: 'Configuración',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
